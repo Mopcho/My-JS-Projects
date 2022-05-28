cf45188096336fed330d70c90908b4823c0a51d2
Mopcho <valeri_petkov01@abv.bg>
2022-05-28 15:34:18 +0300

    Added routing and basic home page

--- Simple Chat App With Express/src/index.js
+++ Simple Chat App With Express/src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const { router } = require('./routes');
 
 const port = 5000;
-
 const app = express();
 
-app.use('hbs',hbs.engine({extname:'hbs'}));
-app.set('view engine', 'hbs');
-app.set('views' , './src/views');
+app.use(express.static('public'));
 
-app.listen(port,`Server listnening on port ${port}`);
+app.engine('hbs',hbs.engine({extname:'hbs'}));
+app.set('view engine', 'hbs');
+app.set('views','./src/views');
+
+app.use(router);
+
+app.listen(port,()=> {console.log(`Server listnening on port ${port}`)});
